ff1e7c10631fd5bf68c427d7b0d066311a1db43e
venci0003 <84270419+venci0003@users.noreply.github.com>
2023-02-22 21:59:05 +0200

    Update index.js

--- JS/JS Syntax Fundamentals - Lab/02. Student Information/index.js
+++ JS/JS Syntax Fundamentals - Lab/02. Student Information/index.js
@@ -3,6 +3,4 @@ console.log(`Name: ${name}, Age: ${age}, Grade: ${averageGrade.toFixed(2)}`);
 }
 <<<<<<< HEAD
 =======
-
-studentInformation("sexs",2,1.123213)
 >>>>>>> 5b3106e5b36233eed15bfc856502360bad8273d8
