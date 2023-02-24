ccd0203f0fd56e97a97ca1683a9825fae02245a7
venci0003 <venci0003@gmail.com>
2023-02-24 18:25:40 +0200

    Uploaded solution "11. Sum First and Last Array Elements" to - JS Syntax Fundamentals - Lab

---
+++ JS/JS Syntax Fundamentals - Lab/11. Sum First and Last Array Elements/index.js
@@ -0,0 +1,11 @@
+function sumFirstAndLastElements(input) {
+    let firstNumber = input[0];
+
+    let lastNumber = input[input.length - 1];
+    
+    let sum = 0;
+
+    sum = firstNumber + lastNumber;
+
+    console.log(sum)
+}
