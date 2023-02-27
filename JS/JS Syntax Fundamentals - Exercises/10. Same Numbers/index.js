a36d487f4f29abac233649e267a5e2c49e03abf6
venci0003 <venci0003@gmail.com>
2023-02-27 19:44:13 +0200

    Uploaded solution "10. Same Numbers" to - JS Syntax Fundamentals - Exercises

---
+++ JS/JS Syntax Fundamentals - Exercises/10. Same Numbers/index.js
@@ -0,0 +1,20 @@
+function sameNumbers(number) {
+  let numberSum = 0;
+  let areSame = true;
+  let oldDigit = 0;
+  let iterations = 0;
+
+  while (number > 0) {
+    let digit = number % 10;
+    numberSum += digit;
+    number /= 10;
+    number = parseInt(number);
+    if (iterations !== 0 && oldDigit !== digit) {
+      areSame = false;
+    }
+    oldDigit = digit;
+    iterations++;
+  }
+  console.log(areSame);
+  console.log(numberSum);
+}
