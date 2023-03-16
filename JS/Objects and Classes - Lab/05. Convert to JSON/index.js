56308d905f49350028d56c5a4553a1d77f2c666e
venci0003 <84270419+venci0003@users.noreply.github.com>
2023-03-16 19:49:48 +0200

    Update index.js

--- JS/Objects and Classes - Lab/05. Convert to JSON/index.js
+++ JS/Objects and Classes - Lab/05. Convert to JSON/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 614a356c41dcde04811d945158b60890df8e0863
 function convert(name,lastName, hairColor){
     let person = {name,lastName,hairColor};
 
@@ -13,7 +9,3 @@ function convert(name,lastName, hairColor){
 
     console.log(JSON.stringify(person));
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 614a356c41dcde04811d945158b60890df8e0863
