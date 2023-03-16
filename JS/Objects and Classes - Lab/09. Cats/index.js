50c1b14f9e5e42839912a16beff8464af1f73aac
venci0003 <84270419+venci0003@users.noreply.github.com>
2023-03-16 19:51:11 +0200

    Update index.js

--- JS/Objects and Classes - Lab/09. Cats/index.js
+++ JS/Objects and Classes - Lab/09. Cats/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function cat(array) {
     class Cat {
       constructor(name, age) {
@@ -17,23 +17,3 @@ function cat(array) {
     }
     cats.forEach((c) => c.meow());
   }
-=======
-function cat(array) {
-    class Cat {
-      constructor(name, age) {
-        this.name = name;
-        this.age = age;
-      }
-      meow() {
-        console.log(`${this.name}, age ${this.age} says Meow`);
-      }
-    }
-    let cats = [];
-    for (const token of array) {
-      let [name, age] = token.split(" ");
-      let cat = new Cat(name, age);
-      cats.push(cat);
-    }
-    cats.forEach((c) => c.meow());
-  }
->>>>>>> 614a356c41dcde04811d945158b60890df8e0863
