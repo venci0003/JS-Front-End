af282b8ba21e66bfd3b2821ada9d66e9fdb55d85
venci0003 <venci0003@gmail.com>
2023-03-22 17:06:48 +0200

    Uploaded solution "02. Show More" to - DOM and Events - Lab

---
+++ JS/DOM and Events - Lab/02. Show More/ShowMore.js
@@ -0,0 +1,9 @@
+function showText() {
+    const anchorTag = document.getElementById("more");
+
+    const spanTag = document.getElementById("text");
+
+    anchorTag.style.display = "none";
+
+    spanTag.style.display = "inline";
+}
