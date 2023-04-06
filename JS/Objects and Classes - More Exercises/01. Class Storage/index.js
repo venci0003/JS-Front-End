72d216939232aacdb6dc8c5db7e6651c23635322
venci0003 <venci0003@gmail.com>
2023-04-06 18:43:45 +0300

    Uploaded solution "01. Class Storage" to - Objects and Classes - More Exercises

---
+++ JS/Objects and Classes - More Exercises/01. Class Storage/index.js
@@ -0,0 +1,27 @@
+class Storage {
+  constructor(capacity) {
+    (this.capacity = capacity), (this.storage = []), (this.totalCost = 0);
+  }
+  addProduct(product) {
+    if (this.capacity - product.quantity >= 0) {
+      this.storage.push(product);
+      this.capacity -= product.quantity;
+      this.totalCost += product.price * product.quantity;
+    }
+  }
+
+  getProducts() {
+    return this.storage.map((product) => JSON.stringify(product)).join("\n");
+  }
+}
+
+let productOne = { name: "Cucamber", price: 1.5, quantity: 15 };
+let productTwo = { name: "Tomato", price: 0.9, quantity: 25 };
+let productThree = { name: "Bread", price: 1.1, quantity: 8 };
+let storage = new Storage(50);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+storage.addProduct(productThree);
+console.log(storage.getProducts());
+console.log(storage.capacity);
+console.log(storage.totalCost);
